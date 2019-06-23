0851c8c87cafef4347112cb8249f5932f1dc8039
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-06-23 16:59:43 +0600

    Solved logout error

--- Code/pages/urls.py
+++ Code/pages/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
 from .views import HomePageView
 
-urlpatterns = [
-    path('', HomePageView.as_view(), name = 'home'),
 
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
 ]
--- Code/pages/views.py
+++ Code/pages/views.py
@@ -2,5 +2,5 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
-class HomePageView(TemplateView):
+class HomePageView(TemplateView): 
     template_name = 'home.html'
