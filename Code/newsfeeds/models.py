ab27962afd5f5de3b34987ab7b202866edf53956
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-06-29 17:57:18 +0600

    Added newsfeed option with crud functionality

--- Code/newsfeeds/models.py
+++ Code/newsfeeds/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
-
-class Newsfeed(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True) 
@@ -12,10 +11,7 @@ class Newsfeed(models.Model):
             get_user_model(),
             on_delete=models.CASCADE,
         )
-
     def __str__(self): 
         return self.title
-
-
     def get_absolute_url(self):
-        return reverse('newsfeed_detail', args=[str(self.id)])
+        return reverse('article_detail', args=[str(self.id)])
--- Code/newsfeeds/urls.py
+++ Code/newsfeeds/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import NewsFeedView
-
-
+from .views import ( 
+    ArticleListView,
+    ArticleUpdateView,
+    ArticleDetailView,
+    ArticleDeleteView, # new
+    ArticleCreateView
+)
 urlpatterns = [
-    path('', NewsFeedView.as_view(), name='newsfeed_list'),
+    path('<int:pk>/edit/',
+         ArticleUpdateView.as_view(), name='article_edit'), # new
+    path('<int:pk>/',
+         ArticleDetailView.as_view(), name='article_detail'), # new
+    path('<int:pk>/delete/',
+         ArticleDeleteView.as_view(), name='article_delete'), # new
+    path('new/', ArticleCreateView.as_view(), name='article_new'), # new
+    path('', ArticleListView.as_view(), name='article_list'),
 ]
