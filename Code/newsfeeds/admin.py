4887a8cf18b1a5def40ff9156008878a7675387a
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-07-13 20:16:10 +0600

    fixed the folder name #26

--- Code/newsfeeds/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin 
-from .models import Article, Overview, PersonalInfo, Education, Job, Accomplishment, Skillset, Skill
-
-
-admin.site.register(Article)
-admin.site.register(PersonalInfo)
--- Code/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254)
