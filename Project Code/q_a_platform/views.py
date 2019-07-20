8e6007221a968ca8da0bcdf52bdaef75dc5e29a1
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-07-21 00:05:33 +0600

    enhancement #9: added detail, update and create view class to the views.py file

--- Project Code/q_a_platform/views.py
+++ Project Code/q_a_platform/views.py
@@ -5,3 +5,19 @@ from django.urls import reverse_lazy # new
 class QArticleListView(ListView): 
     model = QAarticle
     template_name = 'article_list.html'
+
+class QArticleDetailView(DetailView): # new 
+    model = QAarticle
+    template_name = 'article_detail.html'
+
+
+class QArticleUpdateView(UpdateView): # new 
+    model = QAarticle
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+
+class QArticleDeleteView(DeleteView): # new
+    model = QAarticle
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
