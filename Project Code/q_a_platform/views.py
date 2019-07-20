d39ce1b2f1efa944566ef4466a4cbafbc0abf861
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-07-21 00:03:48 +0600

    enhancement #9: added listview class to ehe views.py file

--- Project Code/q_a_platform/views.py
+++ Project Code/q_a_platform/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import UpdateView, DeleteView, CreateView # new from django.urls import reverse_lazy # new
+from .models import QAarticle
+from django.urls import reverse_lazy # new
+class QArticleListView(ListView): 
+    model = QAarticle
+    template_name = 'article_list.html'
