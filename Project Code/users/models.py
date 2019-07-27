704dd390cb9e93129a08e820957d6becb70cd085
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-07-28 02:42:55 +0600

    Feature #5: added profile page functions

--- Project Code/users/models.py
+++ Project Code/users/models.py
@@ -1,8 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 # Create your models here.
 
 class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default= 'default.jpg', upload_to = 'profile.pics')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
