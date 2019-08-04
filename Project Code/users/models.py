2436783c33fa6ea848906ecb23ad768c51ee58ab
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-08-04 06:48:26 +0600

    Enhancement #34: added profile data to the models.py

--- Project Code/users/models.py
+++ Project Code/users/models.py
@@ -6,3 +6,10 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete= models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to = 'profile_pics')
+
+    def __str__(self):
+        return f'{self.user} Profile'
