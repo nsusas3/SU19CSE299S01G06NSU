66071850461abad1e6ee53f8aad28da18d88290b
Sifatul Alam Shohan <sifatul.shohan@northsouth.edu>
2019-08-04 06:55:35 +0600

    Feature #5: Added signals to the signals.py file

--- Project Code/users/urls.py
+++ Project Code/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+
 from .views import SignUpView
-
-
+from django.views.generic import TemplateView
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-]
+    path('signup/', SignUpView.as_view(), name = 'signup'),
+    path('profile/', TemplateView.as_view(template_name="profile.html"), name= 'profile'),
